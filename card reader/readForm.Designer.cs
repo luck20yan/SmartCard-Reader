446dcbcc1f170bcefb7ddd73c4cb97dfde37a55f
yan khachko <luck207869@gmail.com>
2019-07-18 20:16:37 +0300

    Добавьте файлы проекта.

---
+++ card reader/readForm.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace card_reader
+{
+    partial class readform
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.viewer = new System.Windows.Forms.ListBox();
+            this.readbutton = new System.Windows.Forms.Button();
+            this.savebutton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // viewer
+            // 
+            this.viewer.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.viewer.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.viewer.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.viewer.Font = new System.Drawing.Font("HelveticaNeueCyr", 9.749999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.viewer.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.viewer.FormattingEnabled = true;
+            this.viewer.ItemHeight = 15;
+            this.viewer.Location = new System.Drawing.Point(1, 1);
+            this.viewer.Name = "viewer";
+            this.viewer.Size = new System.Drawing.Size(806, 390);
+            this.viewer.TabIndex = 0;
+            // 
+            // readbutton
+            // 
+            this.readbutton.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.readbutton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(55)))), ((int)(((byte)(55)))), ((int)(((byte)(55)))));
+            this.readbutton.FlatAppearance.BorderSize = 0;
+            this.readbutton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(65)))), ((int)(((byte)(65)))), ((int)(((byte)(65)))));
+            this.readbutton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(60)))), ((int)(((byte)(60)))));
+            this.readbutton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.readbutton.Font = new System.Drawing.Font("HelveticaNeueCyr", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.readbutton.ForeColor = System.Drawing.Color.White;
+            this.readbutton.Location = new System.Drawing.Point(0, 391);
+            this.readbutton.Name = "readbutton";
+            this.readbutton.Size = new System.Drawing.Size(751, 59);
+            this.readbutton.TabIndex = 1;
+            this.readbutton.Text = "read";
+            this.readbutton.UseVisualStyleBackColor = false;
+            this.readbutton.Click += new System.EventHandler(this.Readbutton_Click);
+            // 
+            // savebutton
+            // 
+            this.savebutton.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.savebutton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(55)))), ((int)(((byte)(55)))), ((int)(((byte)(55)))));
+            this.savebutton.FlatAppearance.BorderSize = 0;
+            this.savebutton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(65)))), ((int)(((byte)(65)))), ((int)(((byte)(65)))));
+            this.savebutton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(60)))), ((int)(((byte)(60)))));
+            this.savebutton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.savebutton.Font = new System.Drawing.Font("Segoe MDL2 Assets", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.savebutton.ForeColor = System.Drawing.Color.White;
+            this.savebutton.Location = new System.Drawing.Point(748, 391);
+            this.savebutton.Name = "savebutton";
+            this.savebutton.Size = new System.Drawing.Size(59, 59);
+            this.savebutton.TabIndex = 3;
+            this.savebutton.Text = "";
+            this.savebutton.UseVisualStyleBackColor = false;
+            this.savebutton.Click += new System.EventHandler(this.Savebutton_Click);
+            // 
+            // readform
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.ClientSize = new System.Drawing.Size(804, 451);
+            this.Controls.Add(this.savebutton);
+            this.Controls.Add(this.readbutton);
+            this.Controls.Add(this.viewer);
+            this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.Name = "readform";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "card reader";
+            this.Load += new System.EventHandler(this.Readform_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox viewer;
+        private System.Windows.Forms.Button readbutton;
+        private System.Windows.Forms.Button savebutton;
+    }
+}
+
---
+++ card reader/readForm.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using PCSC;
+using PCSC.Iso7816;
+namespace card_reader
+{
+    public partial class readform : Form
+    {
+
+        public readform()
+        {
+            InitializeComponent();
+        }
+
+        private void Readbutton_Click(object sender, EventArgs e)
+        {
+            string id = Readcardid();
+          if (id != "") viewer.Items.Add(id);
+           else viewer.Items.Add("no data");
+
+        }
+        string Readcardid()
+        {
+            SCardContext card = new SCardContext();
+
+            card.Establish(SCardScope.System);
+            string[] readers = card.GetReaders();
+            SCardReaderState status = card.GetReaderStatus(readers[0]);
+            return BitConverter.ToString(status.Atr ?? new byte[0]);
+        }
+
+        private void Savebutton_Click(object sender, EventArgs e)
+        {
+                string filePath = @"";
+
+                SaveFileDialog saveFileDialog = new SaveFileDialog();
+
+                saveFileDialog.InitialDirectory = "c:\\";
+                saveFileDialog.Filter = "txt files (*.txt)|*.txt";
+                saveFileDialog.FilterIndex = 2;
+                saveFileDialog.RestoreDirectory = true;
+
+
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    filePath = saveFileDialog.FileName;
+                }
+
+                if(filePath != @"")
+                {
+                    StreamWriter sw = new StreamWriter(filePath);
+                    foreach (string cardid in viewer.Items)
+                    {
+                    sw.WriteLine(cardid);
+                    }
+                sw.Close();
+                }
+        }
+
+        private void Readform_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
